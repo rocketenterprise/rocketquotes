1615e902be79157f560c2f2516156a0ad862b4ca
Dev <57229351+rocketenterprise@users.noreply.github.com>
2023-10-27 17:34:58 -0400

    update index

--- index.js
+++ index.js
@@ -1,19 +1,16 @@
 document.getElementById("insurance-form").addEventListener("submit", function (e) {
-    e.preventDefault(); // Prevent the form from submitting
+    e.preventDefault();
 
-    // Get user inputs
     const age = document.getElementById("age").value;
     const gender = document.getElementById("gender").value;
     const zipcode = document.getElementById("zipcode").value;
 
-    // Simulate generating quotes (replace with actual quote generation logic)
     const quotes = [
         { provider: "Provider 1", quote: "$100/month" },
         { provider: "Provider 2", quote: "$120/month" },
         { provider: "Provider 3", quote: "$90/month" },
     ];
 
-    // Create and display the pop-up
     const popup = document.getElementById("popup");
     popup.innerHTML = "<h2>Insurance Quotes</h2>";
     quotes.forEach((quote) => {
